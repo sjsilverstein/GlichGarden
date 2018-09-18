37392b77b811c5a934a911efc55768013ee9c60c
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-09-18 16:42:58 -0700

    Splash
    Splash to Main Menu Screen

---
+++ GlichGarden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public float autoLoadNextLevelAfter;
+
+    private void Start()
+    {
+        Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+    }
+
+    public void LoadLevel(string name) {
+        Debug.Log("Load New Level: " + name);
+        Application.LoadLevel(name);
+    }
+    public void QuitRequest() {
+        Debug.Log("Quit Request Recieved");
+        Application.Quit();
+    }
+    public void LoadNextLevel() {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+}
