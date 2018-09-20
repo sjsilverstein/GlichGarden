3ea58590b78efbcb4edad31ab04bad6542315d00
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-09-19 17:08:46 -0700

    FadeIn / PreFab GameSpace
    work on fade in menus and game space.

---
+++ GlichGarden/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour {
+
+    public float fadeInTimeInSeconds;
+    private Image fadePanel;
+    private Color currentColor = Color.black;
+
+    private void Start()
+    {
+        fadePanel = GetComponent<Image>();
+    }
+    private void Update()
+    {
+        if (Time.timeSinceLevelLoad < fadeInTimeInSeconds)
+        {
+            //Fade In
+            float alphaChange = Time.deltaTime / fadeInTimeInSeconds;
+            currentColor.a -= alphaChange;
+            fadePanel.color = currentColor;
+        }
+        else {
+            gameObject.SetActive(false);
+        }
+    }
+}
